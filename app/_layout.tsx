97846647acb342a5677c726c04bdb62e9ede1aca
OnyekwereMichael <onyekweremichael55@gmail.com>
2025-10-27 02:27:00 -0700

    setup and structure

---
+++ app/_layout.tsx
@@ -0,0 +1,31 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
+import 'react-native-reanimated';
+import "../global.css"
+
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+const queryClient = new QueryClient();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      </QueryClientProvider>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
